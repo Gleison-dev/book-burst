e498a07732532594e22bcd92481573fc55701b91
Gleison Ribeiro Gomes <gleisonrgomes@gmail.com>
2024-10-28 11:44:48 -0300

    feat: book messages created

---
+++ src/shared/book.message.js
@@ -0,0 +1,10 @@
+const ERRORS = {
+  ALREADY_EXISTS: "Livro já cadastrado. Tente novamente!",
+  NOT_FOUND: "Livro não encontrado. Tente novamente!",
+};
+
+const SUCESS = {
+  CREATED: "Livro cadastrado com sucesso!",
+  UPDATED: "Livro atualizado com sucesso!",
+  DELETED: "Livro deletado com sucesso!",
+};
